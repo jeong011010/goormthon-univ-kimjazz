541d4e07365011bbd4d0483a572ee3041fe3a265
Kim._.jazz <jeong011010@naver.com>
2025-05-02 21:28:44 +0900

    feat: app.jsx 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,36 @@
 import styled from 'styled-components';
+import { useState, useEffect } from 'react';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+
+
+  useEffect(() => {
+    alert('반가븝니다.');
+  }, []);
+
+  useEffect(() => {
+    if(count>9) alert("와우! 10번이나 눌렀어!");
+  }, [count]);
+
+  return (
+    <Container>
+      <Title>안녕하세요! 저는 김정훈입니다</Title>
+      <Info>웹 풀스택 개발자 지망</Info>
+      <Info>관심 기술: Next.JS, TS, SpringBoot, MySQL</Info>
+      <Link href="https://velog.io/@jeong011010" target="_blank">블로그 보러가기</Link>
+
+      <div style={{ marginTop: '2rem' }}>
+        <h2>나를 눌러준 횟수: {count}회</h2>
+        <button onClick={handleClick}>나를 눌러주세요</button>
+      </div>
+    </Container>
+  );
+}
 
 const Container = styled.div`
   max-width: 500px;
@@ -30,16 +62,5 @@ const Link = styled.a`
     text-decoration: underline;
   }
 `;
-
-function App() {
-  return (
-    <Container>
-      <Title>안녕하세요! 저는 김정훈입니다</Title>
-      <Info>웹 풀스택 개발자 지망</Info>
-      <Info>관심 기술: Next.JS, TS, SpringBoot, MySQL</Info>
-      <Link href="https://velog.io/@jeong011010" target="_blank">블로그 보러가기</Link>
-    </Container>
-  );
-}
 
 export default App;
